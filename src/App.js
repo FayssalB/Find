25fc3d9b97341bba27e9f52f6a0d141a2e11878e
Fayssal Belghazi <faysstoface@hotmail.fr>
2023-02-21 16:59:30 +0100

    starting project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  // Fayssal1
+  // https://api.ipgeolocation.io/ipgeo?apiKey=f08a594da54e48cab93750ee4f229b25&ip=
+  const test = fetch('https://api.ipgeolocation.io/getip')
+  .then(response => response.json())
+  console.log(test);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <p>Lets Go</p>
+        <a className="App-link">
           Learn React
         </a>
+        <button type='button' className='button'>Clique</button>
       </header>
     </div>
   );
